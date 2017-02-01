4483070aa41abb575f88b0b577358de761aeff66
balyam <zolotivan@mail.ru>
2017-02-01 11:33:42 +0600

    KGS branch

--- helpers/getrate.rb
+++ helpers/getrate.rb
@@ -1,4 +1,6 @@
 require 'sinatra/base'
+require_relative 'goldrate'
+helpers GoldRate
 
 module GetRate
 
@@ -9,36 +11,36 @@ require 'json'
 require 'yaml/store'
 
 
+
 kzt_url = URI('http://nationalbank.kz/?docid=747&switch=russian')
 kgs_url = URI('http://www.nbkr.kg/XML/daily.xml')
 gold_url = 'http://www.kitco.com/charts/livegold.html'
 mark_gold = [0.375, 0.583, 0.585, 0.750, 0.916, 0.999]
-showings = {}
+show_kzt = {}
+show_kgs = {}
 @store = YAML::Store.new("db.yml")
 
 
 # puts "Let's get KZT currency rate"
   kzt_page = Net::HTTP.get(kzt_url)
-    html_kzt = Nokogiri::HTML(kzt_page)  
-
+    html_kzt = Nokogiri::HTML(kzt_page) 
     html_kzt.css('.gen7').select do |elt|
        if elt.text.include? ("USD")
         @currency_kzt = elt.next_element.text.strip.to_f  
-        showings[:currency_kzt] = @currency_kzt unless  @currency_kzt.nil?
+        show_kzt[:currency_kzt] = @currency_kzt unless  @currency_kzt.nil?
        end
         
     end
-=begin
 #puts "Let's get KGS currency rate"
-  kgs_page = Net::HTTP.get(kgs_url)
+    kgs_page = Net::HTTP.get(kgs_url)
     html_kgs = Nokogiri::XML(kgs_page)
-    html_kgs.css("Currency").select do |elt|
-        puts elt
-          
-      showings[:currency_kgs] = elt.css('Value').text.strip
-        
+      html_kgs.css("Currency").select do |elt|
+      if elt["ISOCode"] == "USD"
+        @currency_kgs = elt.css('Value').text.sub(',', '.').to_f.round(2)
+        show_kgs[:currency_kgs] = @currency_kgs
+        puts "Let's get KGS"        
+      end        
     end
-=end
 
 #puts "Let's get gold price!" 
   agent = Mechanize.new 
@@ -47,17 +49,22 @@ showings = {}
 
       html_gold.css('div.content-blk span#sp-bid').each do |elt|
         @gold_value = elt.text.strip.delete(",").to_f
-        showings[:gold_value] =  @gold_value    
+        show_kzt[:gold_value], show_kgs[:gold_value] =  @gold_value    
       end
 
-#Prices per gramm of each marking probe for gold
-   mark_gold.each do |probe|
-    showings[:"#{probe}"] = ((@gold_value/31.103) * @currency_kzt * probe).to_i
-  end
+#USD prices per each marking probe for gold
+usd_rate = probe_arr(@gold_value, mark_gold).to_h
 
-#Store values in YAML database db.store
+   usd_rate.each do |key, value|   
+    show_kgs[:"#{key}"] = (value * @currency_kgs).to_i
+    show_kzt[:"#{key}"] = (value * @currency_kzt).to_i
+   end
+
+  #Store values in YAML database db.store
   @store.transaction do
-    @store["rate"] = showings
+    @store["KZT"] = show_kzt
+    @store["KGS"] = show_kgs
+    @store["USD"] = usd_rate
   end
 
 puts "Done!"
--- mainapp.rb
+++ mainapp.rb
@@ -4,16 +4,15 @@ require 'sinatra/base'
 class MainApp < Sinatra::Base
 require 'yaml/store'
 require_relative './helpers/goldrate'
-
 helpers GoldRate
 
   get '/' do
-	@price = get_db("db.yml", "rate")
+	@price = get_db("db.yml", "KZT")
     erb :index
   end
 
   get '/kgz' do 
-  	@price = get_db("db.yml", "rate")
+  	@price = get_db("db.yml", "KGS")
   	erb :kgz
   end
 
