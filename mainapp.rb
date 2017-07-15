5c9408d3d90fdf7822ea83da965063299e089fca
balyam <zolotivan@mail.ru>
2017-07-16 02:51:44 +0600

    Different metatags for pages

--- mainapp.rb
+++ mainapp.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'sinatra/content_for'
+require 'yaml'
 require_relative './helpers/goldrate'
 
 class MainApp < Sinatra::Base
@@ -8,15 +9,18 @@ class MainApp < Sinatra::Base
 
   before do
     @curr_symbols ||= %w(KZT KGS RUB BYN AZN UZS UAH)
+    @yaml_store ||= YAML.load_file(File.join('meta.yml'))
   end
 
   get '/' do
     @price = get_db($redis, 'KZT')
+    @metatag = @yaml_store.fetch('KZT')
     erb :index
   end
 
   get '/:url' do
     @price = get_db($redis, params[:url].upcase)
-    erb :rub
+    @metatag = @yaml_store.fetch(params[:url].upcase)
+    erb :rub    
   end
 end
