68e73c7ea78d7ee5a2f274f931526ca1ac839599
balyam <zolotivan@mail.ru>
2017-07-19 13:02:44 +0600

    End tag fixed

--- mainapp.rb
+++ mainapp.rb
@@ -16,12 +16,18 @@ class MainApp < Sinatra::Base
   get '/' do
     @price = get_db($redis, 'KZT')
     @metatag = @yaml_store.fetch('KZT')
+    @current_url = @price.fetch('url')
     erb :index
+  end
+
+  get '/disclaimer' do
+    erb :disclaimer
   end
 
   get '/:url' do
     @price = get_db($redis, params[:url].upcase)
     @metatag = @yaml_store.fetch(params[:url].upcase)
+    @current_url = @price.fetch('url')
     erb :rub    
   end
 end
